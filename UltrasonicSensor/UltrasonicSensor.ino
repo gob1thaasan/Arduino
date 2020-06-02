8873ce0ae415f79d65f2367672ec61226514ec88
R.U. Gobithaasan <gobithaasan@gmail.com>
2020-06-02 15:31:55 +0800

    inserted comments in the code

--- UltrasonicSensor/UltrasonicSensor.ino
+++ UltrasonicSensor/UltrasonicSensor.ino
@@ -1,5 +1,6 @@
 /*
- * It offers non-contact range detection from 2 cm to 400 cm 
+ * This sketch show the usage of ultrasonic sensor which is a non-contact range detection tool with capability from 2 cm to 400 cm.
+ * A buzzer is added to this code, where when the obstacle is between 9-11cm, the buzzer is turned on.
  */
 
 #define Buzzer 7 // Buzzer Pin is pin 7
@@ -8,7 +9,7 @@
 
 
 void setup() {
-   Serial.begin(9600); // Starting Serial Terminal
+   Serial.begin(9600); // Starting Serial Terminal for reading the distance of the object
    pinMode(TriggerPin, OUTPUT);
    pinMode(EchoPin, INPUT);
    pinMode(Buzzer, OUTPUT);
@@ -17,28 +18,31 @@ void setup() {
 void loop() {
    long TravelTime, cm;
 
-   //clear trigger pin
+   //clear trigger pin for trigger to occur
    digitalWrite(TriggerPin, LOW);
    delayMicroseconds(1);
    
-   //set trigger pin HIGH for 5 microseconds
+   //set trigger pin HIGH for 5 microseconds, then turn off
    digitalWrite(TriggerPin, HIGH);
    delayMicroseconds(5);
    digitalWrite(TriggerPin, LOW);
 
-   // Read the echo pin, return the travel time in microseconds
+   // Read the echo pin, return the travel time of echo in microseconds
    TravelTime = pulseIn(EchoPin, HIGH);
+   
+   //convert the travel time in cm
    cm = TravelTime / 29 / 2;
 
+  //display in serial monitor
    Serial.print("Distance(cm):");
    Serial.print(cm);
    Serial.println();
 
+// setting buzzer on when the object is between 9cm to 11cm
    if (9 < cm && cm < 11 ){
     tone(Buzzer, 1000);
    }
    else 
    noTone(Buzzer);
-   
-   //delay(500);
+
 }
